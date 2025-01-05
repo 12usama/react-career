232489aec07d2fffbe5e62b343ae0f4b743a733d
12usama <usamaalam3999@gmail.com>
2025-01-06 00:57:37 +0600

    third time

--- src/components/Home/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/Home/FeaturedJobs/FeaturedJobs.jsx
@@ -16,7 +16,7 @@ const FeaturedJobs = () => {
          <h2 className='text-6xl text-center'>Featured Jobs {jobs.length} </h2>
             <p className='text-center'>Explore thousands of job opportunities with all the information you need. Its your future</p>
          </div>
-         <div>
+         <div className='grid lg:grid-cols-2 gap-6 lg:p-20 p-10'>
             {
                 jobs.map(job => <Job key={job.id} job={job}></Job>)
             }
--- src/components/Home/FeaturedJobs/Job/Job.jsx
+++ src/components/Home/FeaturedJobs/Job/Job.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
 const Job = ({ job }) => {
-    const { job_title, logo } = job;
+    const { job_title,logo,company_name,remote_or_onsite,location,job_type} = job;
     return (
         <div>
-            <div className="card card-compact bg-base-100 w-96 shadow-xl">
+            <div className="card card-compact bg-base-100 shadow-xl">
                 <figure>
-                    <img
+                    <img className='h-20'
                         src={logo}
                         alt="Shoes" />
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">{job_title}</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
+                    <p>{company_name}</p>
+                    <div className='flex gap-2'>
+                        <button className='px-5 py-2 font-extrabold border rounded border-blue-500'>{remote_or_onsite}</button>
+                        <button className='px-5 py-2 font-extrabold border rounded border-blue-500'>{job_type}</button>
+                    </div>
+                    <div className="card-actions">
                         <button className="btn btn-primary">Buy Now</button>
                     </div>
                 </div>
